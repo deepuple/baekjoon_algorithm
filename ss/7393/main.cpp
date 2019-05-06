408d07516932fcc1e2826fa0ee92a80b9805ac33
deepuple <deepuple@hotmail.com>
2019-05-06 13:59:05 +0000

    modified:   ss/7393/README.md 	modified:   ss/7393/main.cpp

--- ss/7393/main.cpp
+++ ss/7393/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -15,8 +16,7 @@ int main(){
     cin >> tc;
     
     for(int i = 0 ; i < tc ; i++){
-        cin >> n;   
-   
+        cin >> n;
         cout << "#" << i+1 << " " << get_cnt(n) << endl;
     }
 }
@@ -27,6 +27,8 @@ int get_cnt(int a){
     
     if(a < 10){ return 0; }
     
+    memset(dp, 0, sizeof(dp));
+    
     for(int i = 1 ; i < 10 ; i++){
         dp[1][i][1<<i] = 1;
     }
@@ -36,9 +38,9 @@ int get_cnt(int a){
             for(int k = 0 ; k < 1024 ; ++k){
                 
                 if(j == 0 ){
-                    dp[i][0][k|(1<<0)] += dp[i-1][1][k]; //10 <- 1
+                    dp[i][0][k|1] += dp[i-1][1][k]; //10 <- 1
                 }else if(j == 9){
-                    dp[i][9][k|(1<<9)] += dp[i-1][8][k]; //89 <- 8
+                    dp[i][9][k|512] += dp[i-1][8][k]; //89 <- 8
                 }else{
                     dp[i][j][k|(1<<j)] += dp[i-1][j-1][k];
                     dp[i][j][k|(1<<j)] += dp[i-1][j+1][k];
@@ -51,7 +53,7 @@ int get_cnt(int a){
     }
     
     for(int i = 0 ; i < 10 ; ++i){
-        ret += dp[a][i][(1<<10)-1];  
+        ret += dp[a][i][1023];  
         ret %= MOD;
     }
     
