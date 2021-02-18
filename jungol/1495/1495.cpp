76dc7ce48af4103e28163d0f5bff324714977c29
deepuple <deepuple@hotmail.com>
2021-02-18 13:44:11 +0900

    Create 1495.cpp

---
+++ jungol/1495/1495.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int num, i, j;
+int arr[101][101]={0};
+
+int main(){
+    
+    cin >> n;
+
+    num = 1; i = 1, j = 1;
+
+    while(num <= n*n){       
+        while(i < n+1 && j > 0){  
+            arr[i++][j--] = num++;  // ↙
+        }
+        if(i != n+1 && j == 0) { j++; } // ↓ 왼쪽변
+        else { i--; j+=2; } // →  밑변(구석 포함)
+
+        while(i > 0 && j < n+1){  
+            arr[i--][j++] = num++;  // ↗
+        }
+        if(i == 0 && j != n+1) { i++; } // → 윗변
+        else { i+=2; j--; } // ↓ 오른쪽변(구석 포함)
+        
+    }
+
+    for(i = 1; i <=n ; i++){
+        for(j = 1; j <=n ; j++){
+            if(arr[i][j]!=0){
+                cout << arr[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
